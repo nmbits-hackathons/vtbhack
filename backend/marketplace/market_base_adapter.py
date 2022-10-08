cad6bc3c65368db136e83e415f7f1c7d6168c8fb
Denis Zierpka <deniszierpka@gmail.com>
2022-10-08 09:35:22 +0300

    Some changes

--- backend/marketplace/market_base_adapter.py
+++ backend/marketplace/market_base_adapter.py
@@ -3,9 +3,6 @@ from typing import Any, Optional, Union
 from database.models import DataMarketplaceItem, BaseMarketplaceItem
 from database.database_adapter import create_session
 
-# from sqlalchemy import or_
-# from sqlalchemy import and_
-
 
 class MarketDatabaseAdapter:
     @staticmethod
@@ -42,21 +39,3 @@ class MarketDatabaseAdapter:
     def delete_item(item_id: int) -> None:
         with create_session() as session:
             item = session.query(DataMarketplaceItem).filter(DataMarketplaceItem.id == item_id).delete()
-
-
-
-
-    # @staticmethod
-    # def update_user(user_id: int) -> None:
-    #     pass
-
-
-    # @staticmethod
-    # def get_users_list(left:int,right:int) -> Union[BaseUser, None]:
-    #     with create_session() as session:
-    #         data_list = session.query(DataUser)[left:right]
-    #         user_series = UserSeries()
-    #         for data_user in data_list:
-    #             user_series.series.append(BaseUser.from_orm(data_user))
-
-    #     return user_series
